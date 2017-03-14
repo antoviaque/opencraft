32bfcbee8f2e9f85f5f34f5392ac6455130ef479
Xavier Antoviaque <xavier@antoviaque.org>
2017-03-14 14:46:55 -0300

    Refactor registration.js - Follow pattern of instance.js
    Split the long init into `$scope`'ed functions, called by dedicated
    init() functions. This ensure a better separation of the init logic from
    the specialized functions, and make the functions callable from the
    templates without code changes.

--- registration/static/js/src/registration.js
+++ registration/static/js/src/registration.js
@@ -45,167 +45,177 @@ app.config(function($httpProvider) {
 
 // Controllers ////////////////////////////////////////////////////////////////
 
-app.controller('Registration', ['$scope', '$http', 'djangoForm', function($scope, $http, djangoForm) {
+app.controller('Registration', ['$scope', '$http', 'djangoForm',
+    function($scope, $http, djangoForm) {
 
-    // Reference to this controller.
-    var reg = this;
+        var stripeHandler;
 
-    // These fields will be validated on the server via ajax:
-    // - Unique checks can only be done on the server.
-    var serverValidationFields = [
-        'subdomain',
-        'username',
-        'email',
-        'password_confirmation'
-    ];
-
-    // Stripe
-    // TODO: Move key to config
-    var stripeHandler = StripeCheckout.configure({
-        key: 'pk_test_dXfAk69u10kOnoAErjfMOSDC',
-        name: "Open edX Hosting",
-        token: function(token, args) {
-            console.log("Got stripe token: " + token.id);
-        }
-    });
-
-    // TODO: Move price to config
-    $scope.doCheckout = function() {
-        var options = {
-            description: "1x Starter Instance (monthly)",
-            billingAddress: true,
-            amount: 9500,
-            currency: 'EUR',
-            email: $scope.form['email'].$viewValue
+        $scope.init = function() {
+            $scope.init_form();
+            $scope.init_stripe();
         };
-        stripeHandler.open(options);
-    };
 
-    // Automatically launch the checkout upon successful processing the form
-    angular.element(document).ready(function () {
-        if ($scope.form['pk'].$viewValue) {
-            $scope.doCheckout();
-        }
-    });
+        $scope.init_form = function() {
+            // These fields will be validated on the server via ajax:
+            // - Unique checks can only be done on the server.
+            var serverValidationFields = [
+                'subdomain',
+                'username',
+                'email',
+                'password_confirmation'
+            ];
 
-    // Returns a list of all form fields.
-    var getFormFields = function() {
-        return _.keys($scope.form).filter(function(key) {
-            return !/^\$/.test(key);
-        });
-    };
+            // Validate password strength
+            $scope.$watch('registration.password', function() {
+                var validationFeedback = $scope.getValidationFeedback();
+                if (validationFeedback) {
+                    $scope.displayErrors({ password: validationFeedback });
+                }
+            });
 
-    // Returns current password or undefined:
-    // If an existing user accesses the registration form, password fields are not displayed.
-    var getPassword = function() {
-        var passwordField = $scope.form.password;
-        if (passwordField) {
-            return passwordField.$viewValue;
-        }
-        return '';
-    };
+            // Trigger server-side validation.
+            serverValidationFields.forEach(function(field) {
+                $scope.$watch('registration.' + field, _.debounce(function() {
+                    var formField = $scope.form[field];
+                    if (formField && formField.$dirty && formField.$valid) {
+                        $scope.validate();
+                    }
+                }, 500));
+            });
+        };
 
-    // Returns the result of validating current password using zxcvbn.
-    var validatePassword = function() {
-        var password = getPassword();
-        if (password) {
-            var result = zxcvbn(password);
-            // Update password strength field
-            document.getElementById('id_password_strength').value = result.score;
-            return result;
-        }
-        return {};
-    };
+        $scope.init_stripe = function() {
+            // TODO: Move key to config
+            stripeHandler = StripeCheckout.configure({
+                key: 'pk_test_dXfAk69u10kOnoAErjfMOSDC',
+                name: "Open edX Hosting",
+                token: function(token, args) {
+                    console.log("Got stripe token: " + token.id);
+                }
+            });
 
-    // Returns list of suggestions extracted from password validation result.
-    var compileSuggestions = function(validationResult) {
-        var suggestions = validationResult.feedback.suggestions,
-            warning = validationResult.feedback.warning;
-        if (warning) {
-            suggestions.push(warning);
-        }
-        if (_.isEmpty(suggestions)) {
-            // zxcvbn did not produce any feedback for current password,
-            // so fall back on generic error message
-            suggestions.push(
-                'Please use a stronger password: avoid common patterns ' +
-                    'and make it long enough to be difficult to crack.'
-            );
-        }
-        return suggestions;
-    };
+            // Automatically launch the checkout upon successful processing the form
+            angular.element(document).ready(function () {
+                if ($scope.form['pk'].$viewValue) {
+                    $scope.doCheckout();
+                }
+            });
+        };
 
-    // Makes sure each suggestion ends with period.
-    var formatSuggestions = function(suggestions) {
-        return _.map(suggestions, function(suggestion) {
-            if (!suggestion.endsWith('.')) {
-                return suggestion + '.';
+        // TODO: Move price to config
+        $scope.doCheckout = function() {
+            var options = {
+                description: "1x Starter Instance (monthly)",
+                billingAddress: true,
+                amount: 9500,
+                currency: 'EUR',
+                email: $scope.form['email'].$viewValue
+            };
+            stripeHandler.open(options);
+        };
+
+        // Returns a list of all form fields.
+        $scope.getFormFields = function() {
+            return _.keys($scope.form).filter(function(key) {
+                return !/^\$/.test(key);
+            });
+        };
+
+        // Returns current password or undefined:
+        // If an existing user accesses the registration form, password fields are not displayed.
+        $scope.getPassword = function() {
+            var passwordField = $scope.form.password;
+            if (passwordField) {
+                return passwordField.$viewValue;
             }
-            return suggestion;
-        });
-    };
+            return '';
+        };
 
-    // Returns feedback from password validation.
-    reg.getValidationFeedback = function() {
-        var validationResult = validatePassword();
-        if (validationResult && validationResult.score < 2) {
-            var suggestions = formatSuggestions(compileSuggestions(validationResult));
-            // Concatenate suggestions to ensure that all of them are displayed
-            return [suggestions.join(" ")];
-        }
-        return [];
-    };
-
-    // Display the given error messages. Due to bugs in django-angular, we must
-    // ensure that only modified fields that have passed client-side validation
-    // and fields with a $message (i.e.  fields that have already been passed
-    // to setErrors) are passed to setErrors.
-    // https://github.com/jrief/django-angular/pull/260
-    var displayErrors = function(errors) {
-        var fields = getFormFields().filter(function(key) {
-            var field = $scope.form[key];
-            return field.$message || (field.$dirty && field.$valid);
-        });
-        errors = _.pick(errors, fields);
-        djangoForm.setErrors($scope.form, errors);
-    };
-
-    // Validate password strength
-    $scope.$watch('registration.password', function() {
-        var validationFeedback = reg.getValidationFeedback();
-        if (validationFeedback) {
-            displayErrors({ password: validationFeedback });
-        }
-    });
-
-    // Validate the registration form on the server.
-    $scope.validate = function() {
-        var params = {};
-        getFormFields().forEach(function(key) {
-            params[key] = $scope.form[key].$viewValue;
-        });
-        // Ensure that server doesn't validate password strength;
-        // it should only do this when user submits the form.
-        delete params.password_strength;
-        var request = $http.get('/api/v1/registration/register/validate/', {
-            params: params
-        });
-        request.success(displayErrors);
-        request.error(function() {
-            console.error('Failed to validate form');
-        });
-    };
-
-    // Trigger server-side validation.
-    serverValidationFields.forEach(function(field) {
-        $scope.$watch('registration.' + field, _.debounce(function() {
-            var formField = $scope.form[field];
-            if (formField && formField.$dirty && formField.$valid) {
-                $scope.validate();
+        // Returns the result of validating current password using zxcvbn.
+        $scope.validatePassword = function() {
+            var password = $scope.getPassword();
+            if (password) {
+                var result = zxcvbn(password);
+                // Update password strength field
+                document.getElementById('id_password_strength').value = result.score;
+                return result;
             }
-        }, 500));
-    });
+            return {};
+        };
 
-}]);
+        // Returns list of suggestions extracted from password validation result.
+        $scope.compileSuggestions = function(validationResult) {
+            var suggestions = validationResult.feedback.suggestions,
+                warning = validationResult.feedback.warning;
+            if (warning) {
+                suggestions.push(warning);
+            }
+            if (_.isEmpty(suggestions)) {
+                // zxcvbn did not produce any feedback for current password,
+                // so fall back on generic error message
+                suggestions.push(
+                    'Please use a stronger password: avoid common patterns ' +
+                        'and make it long enough to be difficult to crack.'
+                );
+            }
+            return suggestions;
+        };
+
+        // Makes sure each suggestion ends with period.
+        $scope.formatSuggestions = function(suggestions) {
+            return _.map(suggestions, function(suggestion) {
+                if (!suggestion.endsWith('.')) {
+                    return suggestion + '.';
+                }
+                return suggestion;
+            });
+        };
+
+        // Returns feedback from password validation.
+        $scope.getValidationFeedback = function() {
+            var validationResult = $scope.validatePassword();
+            if (validationResult && validationResult.score < 2) {
+                var suggestions = $scope.formatSuggestions($scope.compileSuggestions(validationResult));
+                // Concatenate suggestions to ensure that all of them are displayed
+                return [suggestions.join(" ")];
+            }
+            return [];
+        };
+
+        // Display the given error messages. Due to bugs in django-angular, we must
+        // ensure that only modified fields that have passed client-side validation
+        // and fields with a $message (i.e.  fields that have already been passed
+        // to setErrors) are passed to setErrors.
+        // https://github.com/jrief/django-angular/pull/260
+        $scope.displayErrors = function(errors) {
+            var fields = $scope.getFormFields().filter(function(key) {
+                var field = $scope.form[key];
+                return field.$message || (field.$dirty && field.$valid);
+            });
+            errors = _.pick(errors, fields);
+            djangoForm.setErrors($scope.form, errors);
+        };
+
+        // Validate the registration form on the server.
+        $scope.validate = function() {
+            var params = {};
+            $scope.getFormFields().forEach(function(key) {
+                params[key] = $scope.form[key].$viewValue;
+            });
+            // Ensure that server doesn't validate password strength;
+            // it should only do this when user submits the form.
+            delete params.password_strength;
+            var request = $http.get('/api/v1/registration/register/validate/', {
+                params: params
+            });
+            request.success($scope.displayErrors);
+            request.error(function() {
+                console.error('Failed to validate form');
+            });
+        };
+
+        $scope.init();
+    }
+]);
 
 })();
